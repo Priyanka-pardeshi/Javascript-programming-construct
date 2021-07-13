9157650d98841a304e0b46163acac2602a3844b9
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-07-13 15:09:01 +0530

    Calculating an employee Wage for month

--- EmployeeWageJs.js
+++ EmployeeWageJs.js
@@ -4,6 +4,7 @@ const IS_PPART_TIME_HR=4
 const IS_FULL_TIME_HR=8
 const IS_ABSENT=0
 const EMPWAGE_PER_HR=20
+const NUM_OF_WORKING_DAYS=20
 
 function getWorkingHr(empCheck)
 {
@@ -20,9 +21,13 @@ switch(empCheck)
         
 }
 }
-let empHr=0;
+let totalEmpHr=0;
+for (let day=0;day<=NUM_OF_WORKING_DAYS;day++)
+{
 let empCheck=Math.floor(Math.random()*10)%3;
-empHr=getWorkingHr(empCheck);
-let empWage=empHr*EMPWAGE_PER_HR;
-console.log("Employee wage:"+empWage);
+totalEmpHr+=getWorkingHr(empCheck)
+}
+
+let empWage=totalEmpHr*EMPWAGE_PER_HR;
+console.log("total employee Hr: "+totalEmpHr+" Employee wage:"+empWage);
 
