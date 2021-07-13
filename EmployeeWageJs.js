2800852469bc73372858e60a0ad958f3a47d2f47
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-07-13 16:12:31 +0530

    Checking condition that total Working hours Should less equal to 160

--- EmployeeWageJs.js
+++ EmployeeWageJs.js
@@ -1,33 +1,32 @@
-const IS_PART_TIME=1
-const IS_FULL_TIME=2
-const IS_PPART_TIME_HR=4
-const IS_FULL_TIME_HR=8
-const IS_ABSENT=0
-const EMPWAGE_PER_HR=20
-const NUM_OF_WORKING_DAYS=20
-
-function getWorkingHr(empCheck)
+const IS_PART_TIME = 1
+const IS_FULL_TIME = 2
+const IS_PPART_TIME_HR = 4
+const IS_FULL_TIME_HR = 8
+const EMPWAGE_PER_HR = 20
+const NUM_OF_WORKING_DAYS = 20
+const MAX_HR_IN_MONTH=160
+function getWorkingHr(empCheck) 
 {
-switch(empCheck)
-{
-    case IS_PART_TIME :
-        return IS_PPART_TIME_HR;
-        
-    case IS_FULL_TIME :
-        return IS_FULL_TIME_HR;
-        
-    default : 
-        return 0
-        
+    switch (empCheck) 
+    {
+        case IS_PART_TIME:
+            return IS_PPART_TIME_HR;
+        case IS_FULL_TIME:
+            return IS_FULL_TIME_HR;
+        default:
+            return 0
+    }
 }
-}
-let totalEmpHr=0;
-for (let day=0;day<=NUM_OF_WORKING_DAYS;day++)
+let totalEmpHr = 0;
+let totalWorkingDays=0;
+while(totalEmpHr<=MAX_HR_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
 {
-let empCheck=Math.floor(Math.random()*10)%3;
-totalEmpHr+=getWorkingHr(empCheck)
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHr += getWorkingHr(empCheck)
 }
 
-let empWage=totalEmpHr*EMPWAGE_PER_HR;
-console.log("total employee Hr: "+totalEmpHr+" Employee wage:"+empWage);
+let empWage = totalEmpHr * EMPWAGE_PER_HR;
+console.log("Total Working days: "+totalWorkingDays+" total employee Hr: " + totalEmpHr + " Employee wage:" + empWage);
 
+  
