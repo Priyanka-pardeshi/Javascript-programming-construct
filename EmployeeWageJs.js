a8f57344314a5bc84b1f294af1c36a50d2891df8
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-07-13 14:24:28 +0530

    Checking Daily Employee Wage

--- EmployeeWageJs.js
+++ EmployeeWageJs.js
@@ -1,13 +1,28 @@
+const IS_PART_TIME=1
+const IS_FULL_TIME=2
+const IS_PPART_TIME_HR=4
+const IS_FULL_TIME_HR=8
 const IS_ABSENT=0
-let empCheck=Math.floor(Math.random()*10)%2
-if(empCheck==IS_ABSENT)
+const EMPWAGE_PER_HR=20
+
+let empHr=0
+let empCheck=Math.floor(Math.random()*10)%3
+switch(empCheck)
 {
-    console.log("Employee is Absent")
+    case IS_PART_TIME :
+        empHr=IS_PPART_TIME_HR;
+        break;
+    case IS_FULL_TIME :
+        empHr=IS_FULL_TIME_HR;
+        break;
+    default : 
+        empHr=0
+        break;
 }
-else
-{
-    console.log("Employee is present")
-}
+let empWage=empHr*EMPWAGE_PER_HR;
+console.log("Employee wage:"+empWage);
+
+
 
 
 
