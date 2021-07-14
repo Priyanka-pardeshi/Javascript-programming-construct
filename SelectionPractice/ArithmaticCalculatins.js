d96bb427fa68e030fed7c7b384e82fd3e4a51565
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-07-14 09:07:31 +0530

    Program Using Selection

--- SelectionPractice/ArithmaticCalculatins.js
+++ SelectionPractice/ArithmaticCalculatins.js
@@ -5,17 +5,20 @@ function CalculationOne(valueOne,valueTwo,valueThree)
 }
 function CalculationTwo(valuOne,valuTwo,valuThree)
 {
-    let temp=valuOne/valuTwo;
-    let resultTwo=valuThree+temp;
+    let resultTwo=valuThree+(valuOne/valuTwo);
     console.log(resultTwo);
 }
 function CalculationThree(valueOne,valueTwo,valueThree)
 {
-
+    let resultThree=(valueOne%valueTwo)+valueThree;
+    console.log(resultThree);
 }
 function CalculationFour(valueOne,valueTwo,valueThree)
 {
-
+    let resultFour=(valueOne*valueTwo)+valueThree;
+    console.log(resultFour);
 }
 CalculationOne(2,2,1);
 CalculationTwo(2,4,1);
+CalculationThree(6,2,2)
+CalculationFour(2,2,1)
--- SelectionPractice/InputDayAndMonth.js
+++ SelectionPractice/InputDayAndMonth.js
@@ -1,4 +1,3 @@
-//var day=window.prompt("Enter day:");
 var readlineSync = require('readline-sync');
 var day = readlineSync.question('what will be day? ');
 console.log('day is ' + day);
@@ -8,12 +7,11 @@ console.log('day is ' + monthh);
 //var month=window.prompt("Enter Month");
 //Day and date should be in between 20 march 20 june
 if (monthh == 3 && day > 20 && day <= 31 || monthh == 6 && day >= 20 && day <= 1) {
-    console.log("Valid")
-    //alert("Date is valid");
+    console.log("Date is Valid");
 }
 else if (monthh == 4 || monthh == 5 && day <= 1 && day >= 30) {
     console.log("Valid date");
 }
 else {
-    console.log("Not valid date");
+    console.log("date is invalid");
 }
