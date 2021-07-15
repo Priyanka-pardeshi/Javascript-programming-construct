73f8b44eb0e7988cc70f6dfcf74c31b05d186657
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-07-15 22:05:27 +0530

    Javascript programming using map-Dice roll Problem

--- Map/DiceUsingMap.js
+++ Map/DiceUsingMap.js
@@ -1,2 +1,7 @@
+var readlineSync=require('readline-sync');
+var numb=readlineSync.question('Enter number of time dice sould be roll?');
+for (let i=1;i<=numb;i++)
+{
 var roll=Math.floor((Math.random()*6)+1);
 console.log("after rolling dice result:"+roll);
+}
